01d5ea9b6226dae5ead55084028888d2c5a8bf5c
foguinhoble <137063328+foguinhoble@users.noreply.github.com>
2024-10-21 11:49:36 -0300

    4

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criarCartao{
+    'enfermagem'
+    'o que é rcp'
+    'reanimação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é rcps'
+    'reanimação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é rcp'
+    'resucitação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é rcp'
+    'reanimação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é rcps'
+    'reanimação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é rcp'
+    'resucitação cardiopulmonar '
+}
