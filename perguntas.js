d16dd4f9071559be3999d7c630e8c6fb197bf40d
foguinhoble <137063328+foguinhoble@users.noreply.github.com>
2024-10-28 11:44:01 -0300

    calma

--- perguntas.js
+++ perguntas.js
@@ -6,6 +6,41 @@ criarCartao{
 
 criarCartao{
     'enfermagem'
+    'o que é anatome'
+    'cortar em partes  '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é o ponto médio do corpo'
+    'ponto médio é o meio do corpo, o centro. '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é lateral medial'
+    'meio de uma parte do corpo. algo que está entre duas coisas. '
+}
+
+criarCartao{
+    'enfermagem'
+    'o que é distal'
+    'ponto mais longe do ponto médio. '
+}
+
+criarCartao{
+    'filmes'
+    'o que é '
+    'resucitação cardiopulmonar '
+}
+criarCartao{
+    'enfermagem'
+    'o que é rcp'
+    'reanimação cardiopulmonar '
+}
+
+criarCartao{
+    'enfermagem'
     'o que é rcps'
     'reanimação cardiopulmonar '
 }
