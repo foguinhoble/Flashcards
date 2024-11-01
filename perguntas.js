656aeec1fe33d1e14a5da50ef8daafe395891482
foguinhoble <137063328+foguinhoble@users.noreply.github.com>
2024-11-01 13:36:30 -0300

    ailo

--- perguntas.js
+++ perguntas.js
@@ -1,70 +1,70 @@
 criarCartao{
     'enfermagem'
-    'o que é rcp'
+    'o que é rcp',
     'reanimação cardiopulmonar '
 }
 
 criarCartao{
     'enfermagem'
-    'o que é anatome'
+    'o que é anatome',
     'cortar em partes  '
 }
 
 criarCartao{
     'enfermagem'
-    'o que é o ponto médio do corpo'
+    'o que é o ponto médio do corpo',
     'ponto médio é o meio do corpo, o centro. '
 }
 
 criarCartao{
     'enfermagem'
-    'o que é lateral medial'
+    'o que é lateral medial',
     'meio de uma parte do corpo. algo que está entre duas coisas. '
 }
 
 criarCartao{
     'enfermagem'
-    'o que é distal'
+    'o que é distal',
     'ponto mais longe do ponto médio. '
 }
 
 criarCartao{
-    'filmes'
-    'o que é '
+    'cachorros'
+    'qual o cachorro peludo que tem fucinho curto e é amado por todos?',
     'resucitação cardiopulmonar '
 }
 criarCartao{
-    'enfermagem'
-    'o que é rcp'
+    'cachorros'
+    'o que é um cachorro braquiocefalico',
+    'é um cachorro de fucinho curto '
+}
+
+criarCartao{
+    'cachorros'
+    'o que caes com braquicefalia tem dificuldade de fazer',
+    'correr, brincar, andar durante muito tempo ou ficar muito no sol '
+}
+
+criarCartao{
+    'cachorros'
+    'existe pintcher marrom?',
+    'não. A cor original e unica existente dessa raça é o preto com algumas partes demaradas na coloração marrom '
+}
+
+criarCartao{
+    'cachorros'
+    'o que todos os caes fazem quando feliz',
+    'eles abanam a cauda, mas nem todo cachorro que faz isso está feliz, pode indicar que está bravo e prestes a te atacar. '
+}
+
+criarCartao{
+    'cachorros'
+    '',
     'reanimação cardiopulmonar '
 }
 
 criarCartao{
-    'enfermagem'
-    'o que é rcps'
-    'reanimação cardiopulmonar '
-}
-
-criarCartao{
-    'enfermagem'
-    'o que é rcp'
-    'resucitação cardiopulmonar '
-}
-
-criarCartao{
-    'enfermagem'
-    'o que é rcp'
-    'reanimação cardiopulmonar '
-}
-
-criarCartao{
-    'enfermagem'
-    'o que é rcps'
-    'reanimação cardiopulmonar '
-}
-
-criarCartao{
-    'enfermagem'
+    'cachorros'
     'o que é rcp'
     'resucitação cardiopulmonar '
 }
